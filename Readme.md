a2775536559e4d51e85b31e597a1facb95529f63
YannickFricke <yannickfricke@googlemail.com>
2021-12-08 15:59:59 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,75 @@
+# Dockerized experimental Satisfactory dedicated server
+
+## About this repository
+
+This repository contains everything to start an experimental dockerized dedicated Satisfactory server.
+
+## Early access version
+
+See this repository for the stable (early access) version: [YannickFricke/Satisfactory-DS-Docker](https://github.com/YannickFricke/Satisfactory-DS-Docker)
+
+### Differences to other docker images
+
+Some images on Docker hub are storing the server files in volumes or on the host filesystem. This is not a best practice in the Docker ecosystem as described [here](https://docs.docker.com/develop/develop-images/dockerfile_best-practices/#volume).
+
+Volumes should ONLY be used for user specific configuration / files. The server files are not a part of it and should not be stored in volumes or on the host.
+
+Imagine Coffee Stain Studios breaks the dedicated server software and you wanna play with your friends. Other images would download and store the new server files in the volume. Now every time you start a new container it would read the broken server files and then crash.
+
+It wouldn't even help to completely remove the local image due to that these kind of images are using Shell scripts to download and update the server files dynamically.
+
+With this image this can't happen due to that the server files are stored as immutable data inside the container. Coffee Stain Studios broke the build? No problem! Just use the previous build id to run the old image and play with your friends!
+
+### Do I really need a dedicated server now?
+
+Definetly not! Satisfactory still has the "old" multiplayer mode (through sessions and invites) which is probably less buggy than the dedicated servers.
+
+### When do I need a dedicated server?
+
+There are only three reasons why you need a dedicated server:
+
+- You want to play with your friends and all of you have a bad internet connection
+- Your computer can't handle a second player (due to old computer components)
+- You wanna play in a local network where you don't have internet access
+
+## Requirements
+
+- [Docker](https://docs.docker.com/engine/install/)
+- [docker-compose](https://docs.docker.com/compose/install/) (if you don't wanna use the Docker CLI directly)
+
+The minimum hardware requirements for running the dedicated server which are listed [here](https://satisfactory.fandom.com/wiki/Dedicated_servers#Minimum_requirements).
+
+## How to setup
+
+Download the repository to your computer or server and run the following command:
+
+- docker-compose up
+
+### Further ingame setup
+
+Take a look at the awesome video of Jace (one of the Community Managers @ Coffee Stain Studios) how you set up the server ingame [here](https://youtu.be/Nn-1s87JJxc?t=490).
+
+## Loading a savegame
+
+The folks at Satisfactory wiki have a pretty neat guide on how you can upload the savegame to your server [here](https://satisfactory.fandom.com/wiki/Dedicated_servers#Loading_save_file).
+
+Everything that you need to know regarding this Docker setup is, that you need to place the file in the `shared/SaveGames` directory. `docker-compose` will then mount the directory to the required location inside the container.
+
+As far as I know there will be the possibility that you can upload your savegame directly to the server once it is implemented. Then you won't need the linked guide anymore.
+
+## Known problems
+
+- The dedicated server crashes when you use the `Rebar gun`. It has already been reported [here](https://questions.satisfactorygame.com/post/6178f324831c852052353029).
+- The dedicated server crashes when you try to mass dismantle buildings. It has already been reported [here](https://questions.satisfactorygame.com/post/617877df831c852052352866).
+
+Checkout the Satisfactory Q&A platform for the most recent information regarding server crashes and co [here](https://questions.satisfactorygame.com/search?search=Dedicated).
+
+## How do I report bugs?
+
+If you found a bug in the server / ingame you can report the bug directly to Coffee Stain Studios [here](https://questions.satisfactorygame.com/).
+
+When it comes to the Docker support you can use the [issue tracker](https://github.com/YannickFricke/Experimental-Satisfactory-DS-Docker/issues) of this repository.
+
+## License
+
+This repository is licensed under the MIT license. You can find the exact license in the `LICENSE` file.
---
+++ ci/update.mjs
@@ -0,0 +1,170 @@
+import execa from 'execa';
+import fetch from 'node-fetch';
+
+const previousPublishedBuild = process.env.PREVIOUS_TAG;
+const dockerToken = process.env.DOCKER_TOKEN;
+const githubToken = process.env.GH_TOKEN;
+const forceUpdate = process.env.FORCE_UPDATE;
+const depotName = process.env.DEPOT_NAME || 'public';
+const imageName = 'yfricke/experimental-satisfactory-server';
+const ghcrImageName = 'ghcr.io/yannickfricke/experimental-satisfactory-ds-docker';
+
+function setup() {
+    if (!dockerToken) {
+        throw new Error('DOCKER_TOKEN environment variable is not set');
+    }
+
+    if (!githubToken) {
+        throw new Error('GH_TOKEN environment variable is not set');
+    }
+
+    if (!previousPublishedBuild) {
+        throw new Error('PREVIOUS_TAG environment variable is not set');
+    }
+
+    const dockerLoginResult = execa.sync('docker', ['login', '-u', `yfricke`, '-p', dockerToken], {
+        all: true,
+        stdio: 'inherit'
+    });
+
+    if (dockerLoginResult.failed) {
+        throw new Error(`Failed to login to Docker hub: ${dockerLoginResult.all}`);
+    }
+
+    const githubLoginResult = execa.sync('docker', ['login', 'ghcr.io', '-u', `yannickfricke`, '-p', githubToken], {
+        all: true,
+        stdio: 'inherit'
+    });
+
+    if (githubLoginResult.failed) {
+        throw new Error(`Failed to login to GitHub container registry: ${githubLoginResult.all}`);
+    }
+
+    execa.sync('git', ['config', '--global', 'user.email', 'yannickfricke@googlemail.com']);
+    execa.sync('git', ['config', '--global', 'user.name', 'YannickFricke']);
+}
+
+function buildImage() {
+    console.log(`Building new docker image!`);
+
+    const buildResult = execa.sync('docker', ['build', '-t', `${imageName}:latest`, '.'], {
+        all: true,
+        stdio: 'inherit'
+    });
+
+    if (buildResult.failed) {
+        throw new Error(`Failed to build docker image: ${buildResult.all}`);
+    }
+
+    console.log('Docker image built successfully');
+}
+
+function createTags(latestBuildId) {
+    const ghcrLatestTagResult = execa.sync('docker', ['image', 'tag', `${imageName}:latest`, `${ghcrImageName}:latest`], {
+        all: true,
+        stdio: 'inherit'
+    });
+
+    if (ghcrLatestTagResult.failed) {
+        throw new Error(`Failed to tag docker image: ${ghcrLatestTagResult.all}`);
+    }
+
+    const ghcrLatestBuildTagResult = execa.sync('docker', ['image', 'tag', `${imageName}:latest`, `${ghcrImageName}:${latestBuildId}`], {
+        all: true,
+        stdio: 'inherit'
+    });
+
+    if (ghcrLatestBuildTagResult.failed) {
+        throw new Error(`Failed to tag docker image: ${ghcrLatestBuildTagResult.all}`);
+    }
+
+    const dockerTagResult = execa.sync('docker', ['image', 'tag', `${imageName}:latest`, `${imageName}:${latestBuildId}`], {
+        all: true,
+        stdio: 'inherit'
+    });
+
+    if (dockerTagResult.failed) {
+        throw new Error(`Failed to tag docker image: ${dockerTagResult.all}`);
+    }
+
+    if (forceUpdate === undefined) {
+        const gitTagResult = execa.sync('git', ['tag', `-a`, latestBuildId, '-m', `"Updated to build version ${latestBuildId}"`], {
+            all: true,
+            stdio: 'inherit'
+        });
+
+        if (gitTagResult.failed) {
+            throw new Error(`Failed to create git tag ${latestBuildId}: ${gitTagResult.all}`);
+        }
+    }
+}
+
+function push() {
+    const dockerPushResult = execa.sync('docker', ['image', 'push', '--all-tags', imageName], {
+        all: true,
+        stdio: 'inherit'
+    });
+
+    if (dockerPushResult.failed) {
+        throw new Error(`Failed to push docker image: ${dockerPushResult.all}`);
+    }
+
+    const githubPushResult = execa.sync('docker', ['image', 'push', '--all-tags', ghcrImageName], {
+        all: true,
+        stdio: 'inherit'
+    });
+
+    if (githubPushResult.failed) {
+        throw new Error(`Failed to push docker image: ${githubPushResult.all}`);
+    }
+
+    if (forceUpdate === undefined) {
+        const gitPushResult = execa.sync('git', ['push', 'origin', '--tags'], {
+            all: true,
+            stdio: 'inherit'
+        });
+
+        if (gitPushResult.failed) {
+            throw new Error(`Failed to push GIT tag ${latestBuildId}: ${gitPushResult.all}`);
+        }
+    }
+}
+
+async function checkForUpdates() {
+    try {
+        setup();
+        const latestBuildId = await getLatestBuildId();
+
+        if (previousPublishedBuild === latestBuildId && forceUpdate === undefined) {
+            console.log('No updates available');
+            return;
+        }
+
+        console.log(`New build available: ${latestBuildId}`);
+
+        buildImage();
+        createTags(latestBuildId);
+        push();
+    } catch (error) {
+        console.error(`Unhandled script error: ${error}`);
+        process.exit(1);
+    }
+}
+
+function getLatestBuildId() {
+    return fetch('https://api.steamcmd.net/v1/info/1690800')
+        .then(res => res.json())
+        .then(json => {
+            if (json.status !== 'success') {
+                throw new Error('Failed to get latest build id: Could not fetch information from SteamCMD API');
+            } else {
+                const steamData = json.data['1690800'];
+                const branchData = steamData.depots.branches;
+                const latestBuildId = branchData[depotName].buildid;
+
+                return latestBuildId;
+            }
+        });
+}
+
+checkForUpdates();
